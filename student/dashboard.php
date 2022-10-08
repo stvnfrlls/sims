c967d8ab7b2ba5c53e9e25583d7492be3ce6d2df
stvnfrlls <stvnfrlls@gmail.com>
2022-10-08 14:54:55 +0800

    Added pages
    -student dashboard
    -view grades
    -view announcements
    -view reminders

---
+++ student/dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Dashboard</title>
+</head>
+
+<body>
+    <h1>Student Dashboard</h1>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="auth/login.php">Login</a></li>
+            <li><a href="auth/register.php">Register</a></li>
+            <li><a href="auth/verify.php">Forget Password</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
