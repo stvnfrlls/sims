3021629c8ffe00d9f7c2d577ad08007f1f7fd361
stvnfrlls <stvnfrlls@gmail.com>
2022-10-08 12:07:00 +0800

    Added index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <h1>INDEX PAGE</h1>
+    <nav>
+        <ul>
+            <li><a href="auth/login.php">Login</a></li>
+            <li><a href="auth/register.php">Register</a></li>
+            <li><a href="auth/verify.php">Forget Password</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
