709f43ccbe3759d0481214b3c1dcfeb7730c1bde
stvnfrlls <stvnfrlls@gmail.com>
2022-10-08 18:46:54 +0800

    Added database name

--- utils/database.php
+++ utils/database.php
@@ -1,12 +1,14 @@
 <?php
-    global $conn;
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+global $conn;
 
-    if ($conn->connect_error) {
-        die("Connection Failed: ". $conn->connect_error);
-    }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sis_cdsp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
