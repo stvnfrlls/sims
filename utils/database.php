b36822aadbb22f95dd2d88a8d5daba135c7fa66e
stvnfrlls <stvnfrlls@gmail.com>
2022-10-09 21:31:45 +0800

    Modified to PDO OOP

--- utils/database.php
+++ utils/database.php
@@ -1,19 +1,5 @@
 <?php
 
-global $conn;
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "sis_cdsp";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-}
-
-
 class Database
 {
     private $servername = "localhost";
